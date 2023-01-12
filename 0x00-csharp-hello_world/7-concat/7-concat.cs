4de603daae7c37cbbc3962742858ef8be2560ffd
magicaldave <smokydave@icloud.com>
2023-01-11 22:08:01 -0600

    Add: 7-concat.cs, which sorta concatenates two strings.

---
+++ 0x00-csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Holberton";
+        string str2 = "School";
+        Console.WriteLine("Welcome to {0} {1}!", str1, str2);
+    }
+}
