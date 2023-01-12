b39119695906d880c5da74e4174b6893acb710dd
magicaldave <smokydave@icloud.com>
2023-01-11 23:32:50 -0600

    Add: prints the alphabet, minus q and e.

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char letter = 'a'; letter <='z'; letter++)
+        {
+            if (letter != 'e' && letter != 'q')
+                Console.Write("{0}", letter);
+        }
+    }
+}
