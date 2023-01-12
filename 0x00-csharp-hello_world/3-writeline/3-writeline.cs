9fe3a5f2b1f8b4a6c1d65b11c514e60d7556260f
magicaldave <smokydave@icloud.com>
2023-01-11 20:18:45 -0600

    Edit: added old-style function declarations and changed namespcae in 3-writeline.cs

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+
+namespace _3_writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
