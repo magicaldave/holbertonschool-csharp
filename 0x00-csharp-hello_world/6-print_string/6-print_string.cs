ebe1637c484352fbf155c95166b4b24bb88d857b
magicaldave <smokydave@icloud.com>
2023-01-11 21:51:19 -0600

    Add: 6-print_string.cs, which prints a string three times and most of it a fourth time.

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        Console.WriteLine(str + str + str + '\n' + str.Substring(0, 9));
+    }
+}
