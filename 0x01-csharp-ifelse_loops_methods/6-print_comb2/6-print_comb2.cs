250dad12297694f6237f39ea94ce96fc83d0bda9
magicaldave <smokydave@icloud.com>
2023-01-23 14:06:00 -0600

    Add: 6-print_comb2, which prints unique combinations of two-digit numbers.

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		int tens, ones;
+		for (tens = 0; tens <= 9; ++tens)
+		{
+			for (ones = 1; ones <= 9; ++ones)
+			{
+				if (tens != ones)
+					Console.Write("{0}{1} ", tens, ones);
+			}
+		}
+        }
+    }
+}
