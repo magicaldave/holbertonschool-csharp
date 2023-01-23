79a3387c2c95d020a79ab7ead23c43e6076c5552
magicaldave <smokydave@icloud.com>
2023-01-23 13:25:51 -0600

    Add: 5-print_comb, which prints 0-99 with a newline at the end.

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		int display = 0;
+		for (; display <= 99; ++display)
+			Console.Write("{0}, ", display);
+		if (display == 99)
+			Console.Write("\n");
+        }
+    }
+}
