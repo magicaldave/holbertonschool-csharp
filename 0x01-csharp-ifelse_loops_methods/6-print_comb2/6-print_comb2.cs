0ceb35f9c65b519b2124726ab9c0eb2814151f4f
magicaldave <smokydave@icloud.com>
2023-01-23 15:01:03 -0600

    Edit: 6-print_comb2, now correctly outputs commas and newlines between elements.

--- 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -7,14 +7,19 @@ namespace _6_print_comb2
         static void Main(string[] args)
         {
 		int tens, ones;
-		for (tens = 0; tens <= 9; ++tens)
+		for (tens = 0; tens <= 8; ++tens)
 		{
 			for (ones = 1; ones <= 9; ++ones)
 			{
-				if (tens != ones)
-					Console.Write("{0}{1} ", tens, ones);
+				if (tens < ones)
+				{
+					Console.Write("{0}{1}", tens, ones);
+					if (tens < 8)
+						Console.Write(", ");
+				}
 			}
 		}
+		Console.Write("\n");
         }
     }
 }
