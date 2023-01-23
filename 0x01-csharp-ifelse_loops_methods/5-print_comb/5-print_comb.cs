fc004c11e2034afc3d74b36e62f22aeba6fae8e0
magicaldave <smokydave@icloud.com>
2023-01-23 13:59:47 -0600

    Edit: corrected formatting in 5-print-comb.

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -7,10 +7,11 @@ namespace _5_print_comb
         static void Main(string[] args)
         {
 		int display = 0;
-		for (; display <= 99; ++display)
-			Console.Write("{0}, ", display);
-		if (display == 99)
-			Console.Write("\n");
+		for (; display < 99; ++display)
+		{
+			Console.Write("{0:D2}, ", display);
+		}
+            Console.Write("{0:D2}\n", display);
         }
     }
 }
