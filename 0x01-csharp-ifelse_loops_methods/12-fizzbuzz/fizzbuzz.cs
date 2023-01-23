7bc9adb42533b66950e253b9dc3e8b30054670e8
magicaldave <smokydave@icloud.com>
2023-01-23 16:44:19 -0600

    Add: 12-fizzbuzz, which prints either numbers or specified output depending on the divisibility of an integer.

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/fizzbuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+        static void Main(string[] args)
+        {
+		int Fizzes;
+		for (Fizzes = 1; Fizzes <= 100; ++Fizzes)
+		{
+			switch (Fizzes)
+			{
+			case int n when (n % 15 == 0):
+				Console.Write("Fizzbuzz");
+				break;
+
+			case int n when (n % 5 == 0):
+				Console.Write("Buzz");
+				break;
+
+			case int n when (n % 3 == 0):
+				Console.Write("Fizz");
+				break;
+
+			default:
+				Console.Write(Fizzes);
+				break;
+			}
+			if (Fizzes < 100)
+				Console.Write(", ");
+		}
+        }
+}
