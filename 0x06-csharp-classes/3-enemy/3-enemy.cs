84d669a94c1932d27c8ab36e4028ba514422c308
magicaldave <smokydave@icloud.com>
2023-01-29 17:06:22 -0600

    Add: 3-enemy, which privatizes the zombie's health attribute and adds a function to retrieve it.

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Enemies
+{
+	///<summary>Skeleton Zombie Class, now with health</summary>///
+	class Zombie
+	{
+		///<value>Enemy's total life. Dead at zero or below</value>///
+		private int health;
+		///<summary>Class Constructor; sets health to zero</summary>///
+		public Zombie()
+		{
+			health = 0;
+		}
+		///<summary>Class Constructor; takes arguments and validates them</summary>///
+		public Zombie(int value)
+		{
+			if (value < 0)
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			else
+				health = value;
+		}
+		public int GetHealth()
+		{
+			return this.health;
+		}
+	}
+}
