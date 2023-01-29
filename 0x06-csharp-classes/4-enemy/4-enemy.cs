be93292577e906c798dc85dd0ff108ebbc51bed5
magicaldave <smokydave@icloud.com>
2023-01-29 17:22:17 -0600

    Add: 4-enemy, which adds a new field "name" and a property to match it.

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Enemies
+{
+	///<summary>Skeleton Zombie Class, now with health</summary>///
+	class Zombie
+	{
+		///<value>Enemy's total life. Dead at zero or below</value>///
+		private int health;
+		///<value>Enemy's name. Defaults to none.</value>///
+		private string name = "(No name)";
+		///<summary>Class Constructor; sets health to zero</summary>///
+		public Zombie()
+		{
+			health = 0;
+		}
+		///<summary>Class Constructor; takes arguments and validates them</summary>///
+		public Zombie(int value)
+		{
+			if (value < 0)
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			else
+				health = value;
+		}
+		///<summary>Returns the entity's current health</summary>///
+		public int GetHealth()
+		{
+			return this.health;
+		}
+		///<summary>Getter and Setter for entity's private name.</summary>///
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+	}
+}
