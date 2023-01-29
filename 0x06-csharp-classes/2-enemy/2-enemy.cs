bc5a31b7096c297427ee48c84259ddc6daaa92e9
magicaldave <smokydave@icloud.com>
2023-01-29 16:54:06 -0600

    Edit: allowed 0 health value in 2-enemy

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -15,7 +15,7 @@ namespace Enemies
 		///<summary>Class Constructor; takes arguments and validates them</summary>///
 		public Zombie(int value)
 		{
-			if (value <= 0)
+			if (value < 0)
 				throw new ArgumentException("Health must be greater than or equal to 0");
 			else
 				health = value;
