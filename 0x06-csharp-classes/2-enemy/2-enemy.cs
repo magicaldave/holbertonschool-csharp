e5d8e4c39fb827a9bce7d74b5227ef161c8181ca
magicaldave <smokydave@icloud.com>
2023-01-29 16:52:05 -0600

    Add: 2-enemy, which adds validation for enemy health when instantiated.

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Enemies
+{
+	///<summary>Skeleton Zombie Class, now with health</summary>///
+	class Zombie
+	{
+		///<value>Enemy's total life. Dead at zero or below</value>///
+		public int health;
+		///<summary>Class Constructor; sets health to zero</summary>///
+		public Zombie()
+		{
+				health = 0;
+		}
+		///<summary>Class Constructor; takes arguments and validates them</summary>///
+		public Zombie(int value)
+		{
+			if (value <= 0)
+				throw new ArgumentException("Health must be greater than or equal to 0");
+			else
+				health = value;
+		}
+	}
+}
