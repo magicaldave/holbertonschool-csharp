9a9a8c671fb384e8d15e4deead1b59cf4ee76e43
magicaldave <smokydave@icloud.com>
2023-01-29 15:56:55 -0600

    Add: 1-enemy.cs, which is a zombie class with included health and documentation.

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,14 @@
+﻿namespace Enemies
+{
+	///<summary>Skeleton Zombie Class, now with health</summary>///
+    class Zombie
+    {
+	    ///<value>Enemey's total life. Dead at zero or below</value>///
+	    public int health;
+
+	    public Zombie()
+	    {
+		    health = 0;
+	    }
+    }
+}
