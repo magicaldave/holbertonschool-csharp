3ff099e87ff19b915e9a14448e449b5f280ea7a3
magicaldave <smokydave@icloud.com>
2023-01-29 21:23:57 -0600

    Add: 2-divide_lists, which divides lists with exception handling.

---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+	public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+        {
+		List<int> results = new List<int>();
+
+		for (int i = 0; i < listLength; ++i)
+		{
+			try
+			{
+				results.Add(list1[i] / list2[i]);
+			}
+			catch (DivideByZeroException)
+			{
+				Console.WriteLine("Cannot divide by zero");
+				results.Add(0);
+			}
+			catch (ArgumentOutOfRangeException)
+			{
+				Console.WriteLine("Out of range");
+			}
+		}
+		return results;
+        }
+}
