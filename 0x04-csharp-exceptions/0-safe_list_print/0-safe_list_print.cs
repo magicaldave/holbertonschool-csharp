576bc68342550957d3eb5331899942c26ab25b44
magicaldave <smokydave@icloud.com>
2023-01-29 18:35:01 -0600

    Add: 0x04-Exceptions, README, 0-safe_list_print, which tries to print all elements of a list while catching exceptions.

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int SafePrint(List<int> myList, int n)
+	{
+		int printed;
+		if (n <= 0)
+			return 0;
+		for (printed = 0; printed < n; ++printed)
+		{
+			try
+			{
+				Console.WriteLine("{0}", myList[printed]);
+			}
+			catch (Exception)
+			{
+				return printed;
+			}
+		}
+		return printed;
+
+        }
+}
---
+++ 0x04-csharp-exceptions/README.md
@@ -0,0 +1,12 @@
+This folder contains project files for Csharp - Exceptions.
+They are as follows: 
+
+0)
+
+1)
+
+2)
+
+3)
+
+4)
