c1e1890be0136541360c16f8e63ef0230c10f26e
magicaldave <smokydave@icloud.com>
2023-02-05 16:49:38 -0600

    Add: 0-print_array, rewritten to make liberal use of ternary operators.

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// Creates and prints an array of integers of a given size.
+/// </summary>
+/// <param name="size">The size of the array to create</param>
+/// <returns>The newly created array</returns>
+/// <author>S3kshun-8</author>
+using System;
+using System.Linq;
+class Array {
+	public static int[] CreatePrint(int size)
+	{
+		int[] IntArray;
+
+		// Check if size is greater than 0. If yes, create an array using the range 0 to size-1 and assign it to IntArray.
+		// If size is zero, create an empty array, otherwise initialize IntArray as null.
+		IntArray = size > 0
+			? Enumerable.Range(0, size).ToArray()
+			: size == 0
+			? new int[0]
+			: null;
+
+		// Check if size is greater than 0. If yes, print the elements of IntArray joined with a space.
+		// If size is 0, print a newline character.
+		// If size is less than 0, print "Size cannot be negative".
+		Console.WriteLine(IntArray != null
+				  ? string.Join(" ", IntArray)
+				  : size == 0
+				  ? "\n"
+				  : "Size cannot be negative");
+
+		return IntArray;
+	}
+}
