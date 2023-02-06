2d65f72cf177b89171813d7192093d4a9492d62c
magicaldave <smokydave@icloud.com>
2023-02-05 18:12:18 -0600

    Edit: rearranged conditionals in 1-element_at

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+/// <summary>
+/// Class to retrieve elements from an array.
+/// </summary>
+/// <author>S3kshun-8</author>
 class Array
 {
+    /// <summary>
+    /// Retrieves an element from an array at the given index.
+    /// </summary>
+    /// <param name="array">The array from which to retrieve the element.</param>
+    /// <param name="index">The index of the element to retrieve.</param>
+    /// <returns>The element at the given index, or -1 if the index is out of range.</returns>
     public static int elementAt(int[] array, int index)
-    {
-        if (index >= 0 && index < array.Length)
-        {
-            return array[index];
-        }
-        else
-        {
-            Console.WriteLine("Index out of range");
-            return -1;
-        }
+	{
+		if (index < || index > array.Length)
+		{
+			Console.WriteLine("Index out of range");
+			return -1;
+		}
+		return array[index];
     }
 }
