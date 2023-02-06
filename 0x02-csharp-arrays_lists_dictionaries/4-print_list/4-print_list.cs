e0d0c034439377925a344976ec8b9c54271a4652
magicaldave <smokydave@icloud.com>
2023-02-05 19:31:55 -0600

    Add: 4-print_list, which makes and prints a list of integers, given a specific size.

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Class containing methods for creating and printing a list of integers
+/// </summary>
+/// <author>S3kshun-8</author>
+class List
+{
+    /// <summary>
+    /// Creates and prints a list of integers of a given size.
+    /// </summary>
+    /// <param name="size">The size of the list to be created</param>
+    /// <returns>The newly created list of integers, or null if size is less than 0</returns>
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> numbers = Enumerable.Range(0, size).ToList();
+
+        Console.WriteLine(String.Join(" ", numbers));
+
+        return numbers;
+    }
+}
