04572405662ebc544429d8268f781686a23d7cbf
magicaldave <smokydave@icloud.com>
2023-02-05 18:26:43 -0600

    Add: 2-replace_element.cs, which replaces an element of an array at a given index. Enhanced documentation.

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,22 @@
+﻿using System;
+/// <summary>
+/// Class to manipulate elements in an array.
+/// </summary>
+/// <author>S3kshun-8</author>
+class Array
+{
+    /// <summary>
+    /// Replaces an element of an array at the given index.
+    /// </summary>
+    /// <param name="array">The array in which to replace the element.</param>
+    /// <param name="index">The index of the element to replace.</param>
+    /// <param name="n">The value to replace the element with.</param>
+    /// <returns>The updated array, or the unchanged array if the index is out of range.</returns>
+    public static int[] ReplaceElement(int[] array, int index, int n)
+    {
+        if (index <= 0 || index >= array.Length)
+		Console.WriteLine("Index out of range");
+	array[index] = n;
+        return array;
+    }
+}
