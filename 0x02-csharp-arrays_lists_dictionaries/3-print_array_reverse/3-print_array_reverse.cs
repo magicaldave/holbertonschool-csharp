d13c458730a33f8bb479c7c4bbdf7b2f15e0f48b
magicaldave <smokydave@icloud.com>
2023-02-05 19:06:28 -0600

    Add: 3-print_array_reverse, which prints an array in reverse. Enhanced documentation edition.

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+/// <summary>
+/// Array manipulations, including retrieval, replacement, and reversal.
+/// </summary>
+/// <author>S3kshun-8</author>
+class Array
+{
+    /// <summary>
+    /// Prints all integers of an array, in reverse order.
+    /// </summary>
+    /// <param name="array">The array to be printed in reverse order.</param>
+    public static void Reverse(int[] array)
+    {
+	    if (array != null && array.Length > 1)
+	    {
+		    System.Array.Reverse(array);
+		    Console.WriteLine(String.Join(" ", array));
+		    return;
+	    }
+	    Console.WriteLine();
+    }
+}
