84e4e075b9c34032ad133cf5975540bae30da966
magicaldave <smokydave@icloud.com>
2023-02-05 18:29:24 -0600

    Edit: 2-replace_element, allowed for zero-index without triggering out of range error.

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -14,7 +14,7 @@ class Array
     /// <returns>The updated array, or the unchanged array if the index is out of range.</returns>
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        if (index <= 0 || index >= array.Length)
+        if (index < 0 || index >= array.Length)
 		Console.WriteLine("Index out of range");
 	array[index] = n;
         return array;
